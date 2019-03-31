af727750eb6f2648b0abaf6aacf13a59c5b006b9
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-03-31 00:04:09 -0700

    Updates

---
+++ precache-manifest.b10c086e2dee9614cc0e81535f1d2158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "c59f47857c265d7a4442",
+    "url": "/click-game/static/js/main.983bd24e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c59f47857c265d7a4442",
+    "url": "/click-game/static/css/main.c86781d8.chunk.css"
+  },
+  {
+    "revision": "34d613a9efe07b1406b66345f9d1db8f",
+    "url": "/click-game/index.html"
+  }
+];
